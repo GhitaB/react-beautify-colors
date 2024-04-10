48aa61586929500cf3d4f2b9e765ea37b1d71b6c
GhitaB <ghitabizau27@gmail.com>
2024-04-10 22:32:41 +0300

    WIP add ColorPicker.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import "./App.css";
 
+function ColorPicker(props) {
+  /* eslint-disable react/prop-types */ // TODO: upgrade to latest eslint tooling
+  const { label, id, name } = props;
+  return (
+    <div className="color-picker">
+      <label htmlFor="primarycolor">{label}</label>
+      <input type="color" id={id} name={name} />
+    </div>
+  );
+}
+
 function App() {
+  const data = {
+    label: "Primary color: ",
+    id: "primary-color",
+    name: "primary-color",
+  };
+
+  const data_bla = {
+    label: "Bla: ",
+    id: "bla-color",
+    name: "bla-color",
+  };
+
   return (
     <>
       <div className="container">
@@ -9,11 +32,11 @@ function App() {
           1. <strong>Pick your main color.</strong> It will be used as reference
           to harmonize the other colors.
         </p>
-        <label htmlFor="primarycolor">Primary Color: </label>
-        <input type="color" id="primary-color" name="primary-color" />
+        <ColorPicker {...data} />
         <p>
           2. Now add <strong>secondary colors</strong>.
         </p>
+        <ColorPicker {...data_bla} />
         <p>TODO ADD</p>
         <p>
           3. Ready? Just press <strong>Beautify</strong>.
