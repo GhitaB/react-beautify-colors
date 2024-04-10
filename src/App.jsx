00eb7bc77f474a8b856a537931836e31c5faf767
GhitaB <ghitabizau27@gmail.com>
2024-04-10 22:20:29 +0300

    Some texts.

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,24 @@ function App() {
       <div className="container">
         <h1>React Beautify Colors</h1>
         <p>
-          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-          cillum sint consectetur cupidatat.
+          1. <strong>Pick your main color.</strong> It will be used as reference
+          to harmonize the other colors.
+        </p>
+        <label htmlFor="primarycolor">Primary Color: </label>
+        <input type="color" id="primary-color" name="primary-color" />
+        <p>
+          2. Now add <strong>secondary colors</strong>.
+        </p>
+        <p>TODO ADD</p>
+        <p>
+          3. Ready? Just press <strong>Beautify</strong>.
+        </p>
+        <button>Beautify</button>
+        <p>
+          4. <strong>Copy</strong> the colors.
+        </p>
+        <p>
+          5. Try <strong>again</strong> with other colors.
         </p>
       </div>
     </>
